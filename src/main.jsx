6c3fec2a9fccec34d97de4c734fe7faea9184f6d
Denise Blinn <denise.blinn@gmail.com>
2026-08-10 21:10:32 -0600

    Fix Netlify build - remove top-level await

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import { SharedPlannerProvider } from './context/SharedPlannerContext.jsx'
 import { bootstrapSharedPlanner } from './utils/sharedPlanner.js'
 import './index.css'
 
-await bootstrapSharedPlanner()
+async function startApp() {
+  await bootstrapSharedPlanner()
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><BrowserRouter><SharedPlannerProvider><App /></SharedPlannerProvider></BrowserRouter></React.StrictMode>
-)
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode><BrowserRouter><SharedPlannerProvider><App /></SharedPlannerProvider></BrowserRouter></React.StrictMode>
+  )
+}
+
+startApp()
